f9eade896e74e6217ee250a82336f86bc80268b0
Logan Someone <loganqqq@outlook.com>
2021-08-21 10:32:40 -0400

    add slotcheck

--- api.js
+++ api.js
@@ -78,3 +78,29 @@ function forcecpu(userid, key) {
     }
 
     exports.disccheck = forcedisc()
+
+    function forceslots(userid, key) {
+      const https = require('https')
+      const options = {
+        hostname: 'api.fallback.forcehost.net',
+        port: 443,
+        path: '/userslots.php?id='+userid+'&key='+key,
+        method: 'GET'
+      }
+      
+      const req = https.request(options, res => {
+      
+      
+        res.on('data', d => {
+          return d
+        })
+      })
+      
+       
+      req.on('error', error => {
+        console.error(error)
+      })
+      
+      req.end()
+      }
+      exports.slotcheck = forceslots
