46ccc3fcd524a7bdaf1383659de59f9fcce6c4a0
Logan Someone <loganqqq@outlook.com>
2021-08-20 20:51:00 -0400

    fix some code

--- api.js
+++ api.js
@@ -1,14 +1,14 @@
-function forceram(userid) {
+function forceram(userid, key) {
 const https = require('https')
 const options = {
   hostname: 'api.fallback.forcehost.net',
   port: 443,
-  path: '/userram.php?id='+userid+'&key=5wEWOPekVE',
+  path: '/userram.php?id='+userid+'&key='+key,
   method: 'GET'
 }
 
 const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`)
+
 
   res.on('data', d => {
     return d
