61e99d02924f3dd7b82eb1b5a439de028d07c2cf
Logan Someone <loganqqq@outlook.com>
2021-08-21 09:48:00 -0400

    add disc check

--- api.js
+++ api.js
@@ -52,3 +52,29 @@ function forcecpu(userid, key) {
   exports.cpucheck = forcecpu()
 
 
+  function forcedisc(userid, key) {
+    const https = require('https')
+    const options = {
+      hostname: 'api.fallback.forcehost.net',
+      port: 443,
+      path: '/userdisc.php?id='+userid+'&key='+key,
+      method: 'GET'
+    }
+    
+    const req = https.request(options, res => {
+    
+    
+      res.on('data', d => {
+        return d
+      })
+    })
+    
+     
+    req.on('error', error => {
+      console.error(error)
+    })
+    
+    req.end()
+    }
+
+    exports.disccheck = forcedisc()
