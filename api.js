d0858b3c579b482e6af13c404985afa99cd805ec
Logan Someone <loganqqq@outlook.com>
2021-08-21 09:44:17 -0400

    add cpucheck

--- api.js
+++ api.js
@@ -24,5 +24,31 @@ req.end()
 }
 exports.ramcheck = forceram()
 
+function forcecpu(userid, key) {
+  const https = require('https')
+  const options = {
+    hostname: 'api.fallback.forcehost.net',
+    port: 443,
+    path: '/usercpu.php?id='+userid+'&key='+key,
+    method: 'GET'
+  }
+  
+  const req = https.request(options, res => {
+  
+  
+    res.on('data', d => {
+      return d
+    })
+  })
+  
+   
+  req.on('error', error => {
+    console.error(error)
+  })
+  
+  req.end()
+  }
+
+  exports.cpucheck = forcecpu()
 
 
